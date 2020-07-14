4cda19c59c4ccafd50f24f9e6665bc3494a1426d
timattt <timattt@gmail.com>
2020-07-14 22:29:48 +0300

    Lexical analyzer is added. With only function "tokenize".

---
+++ LexicalAnalyzer.py
@@ -0,0 +1,33 @@
+operators = ['(', ')', '*', '/', '+', '-']
+delimiters = [' ', '\t', '\n']
+
+def tokenize(text:str) -> list:
+	""" This method tokenizes string into operators (as chars) and numbers (as ints) """
+
+	# to ensure that the last number will be taken 
+	text += delimiters[0]
+	
+	# some nice vars
+	result = []
+	raw_number = ""
+	
+	for val in text:
+		# conditions
+		isOperator = val in operators
+		isDelimiter = val in delimiters
+		isNumber = val.isdigit()
+
+		# if it is operator
+		if isOperator:
+			result.append(val)
+			
+		# if it may be number
+		if isNumber:
+			raw_number += val
+		
+		# flush number if it is ending
+		if (isOperator or isDelimiter) and len(raw_number) > 0:
+			result.append(int(raw_number))
+			raw_number = ""
+	
+	return result
