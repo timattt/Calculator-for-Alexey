14a74be013ff72377eecf90d16a4105f3a1fb2ae
synthMoza <eganian.aa@phystech.edu>
2020-07-20 02:58:37 +0300

    Fixed Starter.py (a bit)

--- Starter.py
+++ Starter.py
@@ -5,20 +5,20 @@ import SyntaxAnalyzer
 def welcome_print() -> None:
     """ Just a beatiful welcome sign :) """
     print("=======================================")
-    print("\tEasyCalculatorPy v.0.1")
+    print("\tEasyCalculatorPy v.0.2")
     print("=======================================")
 
 welcome_print()
 
 #Input expression
 print("Enter the statement to solve (avalible operations: '+', '-', '*', '/', round brackets): ")
-test = str(input())
+input_expression = str(input())
 
 #Debug print
 #print(LexicalAnalyzer.tokenize(test))
 
 #Tokenize the string, build a tree
-builder = SyntaxAnalyzer.TreeBuilder(LexicalAnalyzer.tokenize(test))
+builder = SyntaxAnalyzer.TreeBuilder(LexicalAnalyzer.tokenize(input_expression))
 builder.G()
 
 top = builder.ancestors[0]
